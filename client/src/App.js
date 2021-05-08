9f3edc9005cb455231f7db5965c0feb7f17fb504
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-05-08 23:22:00 +0530

    Set up react router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Landing />
-    </>
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Router>
   );
 };
 
