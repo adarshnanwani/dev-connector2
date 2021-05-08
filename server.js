ee7024d5166dd4d22de16b215e524d21ddf70195
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-05-08 23:09:44 +0530

    Add DB connection and APIs.

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Define Route
+const authRoute = require('./routes/api/auth');
+const postsRoute = require('./routes/api/posts');
+const profileRoute = require('./routes/api/profile');
+const usersRoute = require('./routes/api/users');
+
 app.get('/', (req, res) => res.send('DevConnector API running.'));
 
+// Mount Route
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/posts', postsRoute);
+
+// Setup port number
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
