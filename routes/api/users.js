b04f706ccb030d1a773540b72254c81f68de1e54
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-05-08 23:09:45 +0530

    Return jwt.

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../../models/User');
 
 // @route   GET api/users
@@ -31,7 +33,7 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).send({
+        return res.status(400).send({
           errors: [
             {
               msg: 'User already exists',
@@ -55,11 +57,23 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      res.send('User registered');
-
       // Return jsonwebtoken
 
-      res.send('Users route');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
