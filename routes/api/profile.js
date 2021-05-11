7717122d0d047422c320137e9e7383bc5ec4cc24
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2021-05-11 21:58:34 +0530

    Fiz get all profiles API method to return avatar.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -112,7 +112,9 @@ router.post(
 // @access  Public
 router.get('/', async (req, res) => {
   try {
-    const profiles = await Profile.find().populate('user'[('name', 'avatar')]);
+    const profiles = await Profile.find().populate('user', [
+      ('name', 'avatar'),
+    ]);
     return res.json(profiles);
   } catch (err) {
     console.error(err.message);
